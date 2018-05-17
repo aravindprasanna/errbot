20438deef89ca21060c8b19602d70d74d5c40437
Guillaume Binet <gbinet@argo.ai>
2018-05-17 18:39:59 -0400

    temp

--- errbot/templating.py
+++ errbot/templating.py
@@ -1,16 +1,18 @@
 import logging
 import os
+from errbot.plugin_info import PluginInfo
 from jinja2 import Environment, FileSystemLoader
 from bottle import TEMPLATE_PATH
+from pathlib import Path
 
 log = logging.getLogger(__name__)
 
 
-def make_templates_path(root):
-    return os.path.join(root, 'templates')
+def make_templates_path(root: Path):
+    return root / 'templates'
 
 
-system_templates_path = make_templates_path(os.path.dirname(__file__))
+system_templates_path = str(make_templates_path(Path(__file__).parent))
 template_path = [system_templates_path]
 TEMPLATE_PATH.insert(0, system_templates_path)  # for views
 env = Environment(loader=FileSystemLoader(template_path),
@@ -23,15 +25,16 @@ def tenv():
     return env
 
 
-def make_templates_from_plugin_path(plugin_path):
-    return make_templates_path(os.path.dirname(plugin_path))
+def make_templates_from_plugin_path(pluginfo_path: Path):
+    return make_templates_path(pluginfo_path.parent)
 
 
-def add_plugin_templates_path(path):
+def add_plugin_templates_path(plugin_info: PluginInfo):
     global env
-    tmpl_path = make_templates_from_plugin_path(path)
-    if os.path.exists(tmpl_path):
+    tmpl_path = make_templates_from_plugin_path(plugin_info.location)
+    if tmpl_path.exists():
         log.debug("Templates directory found for this plugin [%s]" % tmpl_path)
+        tmpl_path = str(tmpl_path)
         template_path.append(tmpl_path)  # for webhooks
         TEMPLATE_PATH.insert(0, tmpl_path)  # for webviews
         # Ditch and recreate a new templating environment
@@ -40,10 +43,11 @@ def add_plugin_templates_path(path):
     log.debug("No templates directory found for this plugin [Looking for %s]" % tmpl_path)
 
 
-def remove_plugin_templates_path(path):
+def remove_plugin_templates_path(plugin_info: PluginInfo):
     global env
-    tmpl_path = make_templates_from_plugin_path(path)
+    tmpl_path = make_templates_from_plugin_path(plugin_info.location)
     if tmpl_path in template_path:
+        tmpl_path = str(tmpl_path)
         template_path.remove(tmpl_path)  # for webhooks
         TEMPLATE_PATH.remove(tmpl_path)  # for webviews
         # Ditch and recreate a new templating environment
